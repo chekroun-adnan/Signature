6337843e1d9f408ae8390b046f5d86676057ad06
Chekroun Adnan <adnanchekroun00@gmail.com>
2025-07-26 17:45:22 +0100

    Services

---
+++ src/main/java/com/test/test/Services/DocumentService.java
@@ -0,0 +1,50 @@
+package com.test.test.Services;
+
+import com.test.test.Entities.Document;
+import com.test.test.Repository.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class DocumentService {
+
+    @Autowired
+    private final DocumentRepository documentRepository;
+
+    @Value("${filePath}")
+    private String basePath;
+
+    public DocumentService(DocumentRepository documentRepository) {
+        this.documentRepository = documentRepository;
+    }
+
+    public String uploadDocument (String title, MultipartFile multipartFile){
+        File dir = new File(basePath);
+        if (!dir.exists()){
+            dir.mkdirs();
+        }
+        Path path = Path.of(basePath + File.separator + title);
+        if(Files.exists(path)){
+            return "Exist";
+        }
+        try{
+            multipartFile.transferTo(path.toFile());
+
+            Document document =new Document();
+            document.setTitle(title);
+            document.setDocumentType(multipartFile.getContentType());
+            documentRepository.save(document);
+            return "CREATED";
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return "FAILED";
+        }
+    }
+}
---
+++ src/main/java/com/test/test/Services/UserService.java
@@ -0,0 +1,60 @@
+package com.test.test.Services;
+
+import com.test.test.Entities.User;
+import com.test.test.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser (User user){
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers (){
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(Long user_id){
+        User existingUser = userRepository.findById(user_id)
+                .orElseThrow(()-> new RuntimeException("User Not Found With The ID : " + user_id));
+        userRepository.delete(existingUser);
+    }
+
+    public User getUser (Long user_id){
+        User existingUser = userRepository.findById(user_id)
+                .orElseThrow(()-> new RuntimeException("User Not Found with the Id " + user_id));
+        return existingUser;
+    }
+
+    public void updateUser (User user){
+        if (user.getUser_id() != null) {
+            Optional<User> existingUserOpt = userRepository.findById(user.getUser_id());
+
+            if (existingUserOpt.isPresent()) {
+                User existingUser = existingUserOpt.get();
+                 existingUser.setNom(user.getNom());
+                 existingUser.setPrenom(user.getPrenom());
+                 existingUser.setEmail(user.getEmail());
+                 existingUser.setAdresse(user.getAdresse());
+                 existingUser.setCode(user.getCode());
+                 existingUser.setPass(user.getPass());
+                 existingUser.setTelephone(user.getTelephone());
+                 existingUser.setDate_naissance(user.getDate_naissance());
+                 userRepository.save(existingUser);
+            }
+        }
+    }
+}
